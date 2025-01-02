7a76de88bbca5e6e2238c6a88b771512d16ca7ec
lanang <lanangary10@gmail.com>
2025-01-02 23:08:22 +0800

    header animation

--- www/app/themes/orcanation/src/JuiceBox/Components/Header/index.js
+++ www/app/themes/orcanation/src/JuiceBox/Components/Header/index.js
@@ -44,26 +44,27 @@ jQuery($ => {
 
         window.scrollAnchorTo(hash, $speed);
     });
+    let mainHeader = document.querySelector('#main-header');
 
-    // let mainHeader = document.querySelector('#main-header');
+    // add scroll class to header when scrolling up and remove when scrolling down and at top add top class
+    let lastScroll = 0;
+    window.addEventListener('scroll', () => {
+        let currentScroll = window.scrollY;
 
-    // // add scroll class to header when scrolling up and remove when scrolling down and at top add top class
-    // let lastScroll = 0;
-    // window.addEventListener('scroll', () => {
-    //     let currentScroll = window.scrollY;
+        if (currentScroll > lastScroll) {
+            mainHeader.classList.add('scroll-down');
+            mainHeader.classList.remove('scroll-up');
+        } else if (currentScroll < lastScroll) {
+            mainHeader.classList.add('scroll-up');
+            mainHeader.classList.remove('scroll-down');
+        }
 
-    //     if (currentScroll > lastScroll) {
-    //         mainHeader.classList.add('scroll');
-    //     } else {
-    //         mainHeader.classList.remove('scroll');
-    //     }
+        if (currentScroll === 0) {
+            mainHeader.classList.add('top');
+        } else {
+            mainHeader.classList.remove('top');
+        }
 
-    //     if (currentScroll === 0) {
-    //         mainHeader.classList.add('top');
-    //     }
-
-    //     lastScroll = currentScroll;
-    // });
-
-    
+        lastScroll = currentScroll;
+    });
 });
