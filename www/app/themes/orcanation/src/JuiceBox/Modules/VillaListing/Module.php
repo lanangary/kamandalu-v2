c5f86decc6217115cee5d48d25f85479eda5912b
lanang <lanangary10@gmail.com>
2025-01-05 12:44:41 +0800

    singgle villas

--- www/app/themes/orcanation/src/JuiceBox/Modules/VillaListing/Module.php
+++ www/app/themes/orcanation/src/JuiceBox/Modules/VillaListing/Module.php
@@ -8,14 +8,16 @@ class Module extends JBModule
 {
     public function processModule()
     {
-        $post_type = $this->module['post_type'];
+        $post_type = $this->module['post_type'] == 'villa' ? 'villas' : $this->module['post_type'];
 
-        $module_featured = $post_type == 'villa' ? $this->module['villa'] : $this->module['offer'];
+        $module_featured = $post_type == 'villas' ? $this->module['villa'] : $this->module['offer'];
 
         // Extract IDs using array_map
         $featured_id = array_map(function($featured) {
             return $featured->ID;
         }, $module_featured);
+
+
 
         // Query for featured team members
         $argsfeatured = [
@@ -24,7 +26,8 @@ class Module extends JBModule
             'post__in' => $featured_id,
             'orderby' => 'post__in',
         ];
+   
         $this->module['select_item'] = Timber::get_posts($argsfeatured);
-
+        
     }
 }
