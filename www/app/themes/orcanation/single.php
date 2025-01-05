1261ee36372da9db87e92823063fa37b2f192e22
lanang <lanangary10@gmail.com>
2025-01-05 19:37:58 +0800

    culinary single

--- www/app/themes/orcanation/single.php
+++ www/app/themes/orcanation/single.php
@@ -9,9 +9,26 @@
  * @since    Timber 0.1
  */
 
+use Timber\Timber;
+
 $context = Timber::get_context();
 $post = Timber::query_post(false, "\\JuiceBox\\Core\\Post");
 
 $context['post'] = $post;
+if($post->post_type == 'culinary') {
+    $args = array(
+        'post_type' => 'culinary',
+        'posts_per_page' => 3,
+        'post__not_in' => array($post->ID),
+        'orderby' => 'rand'
+    );
+
+    $related_post = Timber::get_posts($args);
+
+    $context['related_post'] = $related_post;
+
+
+
+} 
 
 Timber::render(["single--{$post->ID}.twig", "single--{$post->post_type}.twig", "single.twig"], $context);
